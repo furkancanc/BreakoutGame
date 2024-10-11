4709cc20c27d4feb26d8d051bfb80c2772e31f31
furkancanc <fcc.ceylan@gmail.com>
2024-10-11 21:06:40 +0300

    Basic Window

---
+++ constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Code for a "breakout" game
+// Based on a talk by Vittorio Romeo
+// Uses the SFML graphics library
+
+struct constants
+{
+	static constexpr int window_width{ 520 };
+	static constexpr int window_height{ 450 };
+};
